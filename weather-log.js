788cb01f0f318203f2307621bac7bf3d44869762
ledes2mx <ledes2mx@dukes.jmu.edu>
2021-11-01 22:11:51 -0400

    changed the other get enpoint

--- weather-log.js
+++ weather-log.js
@@ -65,7 +65,7 @@ service.get('/log', (request, response) => {
 });
 
 
-service.get('/log/:day/:month/:year', (request, response) => {
+service.get('/specf-log/:day/:month/:year', (request, response) => {
     const parameters = [
         parseInt(request.params.day),
         parseInt(request.params.month),
@@ -106,7 +106,7 @@ service.post('/log', (request, response) => {
             request.body.weather,
         ];
 
-        const query = 'INSERT INTO logs(day, month, year, temp, weather) VALUES (?, "?", ?, ?, "?")';
+        const query = 'INSERT INTO logs(day, month, year, temp, weather) VALUES (?, ?, ?, ?, ?)';
         connection.query(query, parameters, (error, result) => {
             if (error) {
                 response.status(500);
